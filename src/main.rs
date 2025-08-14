8db5651a672fca2bf1497ed2936878e628e65ab2
Edward Li <edward.li.jhu@gmail.com>
2025-08-14 18:08:35 -0400

    Modify component colors and portfolio formatting

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn Home() -> Element {
 #[component]
 fn Navbar() -> Element {
     rsx! {
-        nav { class: "navbar bg-base-100 shadow-sm",
+        nav { class: "navbar bg-base-200 shadow-sm",
             id: "navbar",
             Link { class: "btn btn-ghost text-xl",
                 to: Route::Home {},
@@ -100,11 +100,11 @@ pub fn Timeline() -> Element {
                         }
                     }
                 }
-                div { class: "timeline-start mb-10 md:text-end max-w-xl",
+                div { class: "list timeline-start mb-10 md:text-end max-w-xl",
                     time { class: "font-mono italic", "Dec 2021 to Present" }
                     div { class: "text-lg font-black", "Ed Li, LLC" }
                     div { class: "text-lg font-bold", "Software Engineer" }
-                    ul { class: "list bg-base-100 rounded-box shadow-md",
+                    ul { class: "list bg-base-200 rounded-box shadow-md mt-4",
                         li { class: "list-row", "Designing and developing a web-based incremental simulation game using the open-source Bevy engine in Rust" }
                         li { class: "list-row", "Designing and developing a cross-platform workout tracker application using Dioxus, Axum, and SurrealDB in Rust" }
                         li { class: "list-row", "Designed and developed a progressive web application using React, Ruby on Rails, GraphQL, and PostgreSQL to facilitate the listing and sale of digital art published under an artist collective on the WAX blockchain" }
@@ -133,7 +133,7 @@ pub fn Timeline() -> Element {
                     time { class: "font-mono italic", "Nov 2020 to Dec 2021" }
                     div { class: "text-lg font-black", "DIA Associates" }
                     div { class: "text-md font-bold", "Data Analyst" }
-                    ul { class: "list bg-base-100 rounded-box shadow-md",
+                    ul { class: "list bg-base-200 rounded-box shadow-md mt-4",
                         li { class: "list-row", "Performed A/B testing on top credit card issuer’s specialized offers to help marketing teams implement better customer targeting and offer terms, thereby increasing customer engagement and retention" }
                         li { class: "list-row", "Identified likely-to-attrite small business and corporate credit card customers to prioritize efforts of outbound attrition prevention team, resulting in hundreds of additional customers retained per month" }
                         li { class: "list-row", "Identified high-spend-potential customers by analyzing early tenure behavior and firmographics, prioritizing efforts of account development team" }
@@ -162,7 +162,7 @@ pub fn Timeline() -> Element {
                     time { class: "font-mono italic", "May 2018 to Oct 2020" }
                     div { class: "text-lg font-black", "Delphi, LLC" }
                     div { class: "text-md font-bold", "Principal Consultant" }
-                    ul { class: "list bg-base-100 rounded-box shadow-md",
+                    ul { class: "list bg-base-200 rounded-box shadow-md mt-4",
                         li { class: "list-row", "Designed and oversaw the implementation of an economically motivated peer-to-peer monetary asset exchange network for a startup seeking to enhance its existing production software" }
                         li { class: "list-row", "Critiqued and prepared economics expert testimony and reports in preparation for corporate litigation defense, resulting in the court dismissing the majority of the plaintiffs’ claims" }
                     }
@@ -188,7 +188,7 @@ pub fn Timeline() -> Element {
                     time { class: "font-mono italic", "Nov 2017 to May 2018" }
                     div { class: "text-lg font-black", "Atana" }
                     div { class: "text-md font-bold", "Vice President of Product" }
-                    ul { class: "list bg-base-100 rounded-box shadow-md",
+                    ul { class: "list bg-base-200 rounded-box shadow-md mt-4",
                         li { class: "list-row", "Oversaw the design of a blockchain-based platform for managing and monetizing electronic health records" }
                         li { class: "list-row", "Presented the platform to investors and advisors for a successful seed round that resulted in $500K funding" }
                         li { class: "list-row", "Facilitated the Company’s entrance in several entrepreneurship and technology competitions, resulting in the receipt of awards, grants, and monetary prizes from ETHDenver, the Ralph S. O’Connor Fund, J.P Morgan, and AARP Foundation" }
@@ -215,7 +215,7 @@ pub fn Timeline() -> Element {
                     time { class: "font-mono italic", "Jun 2017 to Aug 2017" }
                     div { class: "text-lg font-black", "J.P. Morgan" }
                     div { class: "text-md font-bold", "Investment Banking Credit Risk Summer Analyst" }
-                    ul { class: "list bg-base-100 rounded-box shadow-md",
+                    ul { class: "list bg-base-200 rounded-box shadow-md mt-4",
                         li { class: "list-row", "Determined the creditworthiness of asset management firms using both internal and industry-standard rating models" }
                         li { class: "list-row", "Presented a quarterly portfolio review of asset management clients to managing directors" }
                     }
@@ -243,7 +243,7 @@ pub fn Timeline() -> Element {
                     div { class: "text-md font-bold", "Chief Research Assistant for Prof. Steve H. Hanke" }
                     div { class: "text-md font-bold", "Croft Scholarship Fellow" }
                     div { class: "text-md font-bold", "Teaching Assistant" }
-                    ul { class: "list bg-base-100 rounded-box shadow-md",
+                    ul { class: "list bg-base-200 rounded-box shadow-md mt-4",
                         li { class: "list-row", "Independently evaluated potential investments and presented theses to fund managers using a combination of a proprietary discounted cash flow approach, Monte Carlo statistical analysis, and proxy-statement review" }
                         li { class: "list-row", "Led several teams of research assistants on various research projects, including analyzing country-level monetary and financial statistics and monitoring indicators of currency health for the Troubled Currencies Project" }
                         li { class: "list-row", "Published original research papers in academic journals and business magazines, including the Journal of Applied Corporate Finance, on topics ranging from currency board orthodoxy to international trade wars" }
@@ -259,9 +259,9 @@ pub fn Timeline() -> Element {
 #[component]
 pub fn Portfolio() -> Element {
     rsx! {
-        div {
-            id: "blog",
-            div { class: "card bg-base-400 w-96 shadow-sm",
+        div { class: "flex flex-row p-4 gap-4",
+            id: "portfolio",
+            div { class: "card bg-base-200 w-96 shadow-sm",
                 div { class: "card-body",
                     h2 { class: "card-title", "Logger" }
                     p {
@@ -275,7 +275,7 @@ pub fn Portfolio() -> Element {
                     }
                 }
             }
-            div { class: "card bg-base-400 w-96 shadow-sm",
+            div { class: "card bg-base-200 w-96 shadow-sm",
                 div { class: "card-body",
                     h2 { class: "card-title", "PDF Parser" }
                     p {
